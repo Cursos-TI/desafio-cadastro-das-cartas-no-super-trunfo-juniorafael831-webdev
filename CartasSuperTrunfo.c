2d40a5d73a58fcc1ade7cf4743616f42bf7ddd10
juniorafael831-webdev <juniorafael831@gmail.com>
2025-08-28 19:44:45 -0300

    Cartas Supertriunfo HARD.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
-// Declaração das Variaveis
+// Declaração da struct
 struct Cidade {
+    //DECLARÇÃO DE VARIAVEIS. USO DO STRUCT PARA FICAR MELHOR ESTRUTURADO
+    
     char Estado;
     char Carta[10];
     char Codigo[10];
     char Nome[50];
     int Habitantes;
     float Area;
-    float PIB;   // PIB em bilhões
+    float PIB;   
     int Pontos;
+    float Densidade;
+    float PIBperCapita;
+    float SuperPoder;
 };
 
 int main() {
     struct Cidade c1, c2;
+
+    //CHAMA AS VARIAVEIS 
 
     // ---- Cadastro da primeira cidade ----
     printf("Digite o Estado em uma letra: ");
     scanf(" %c", &c1.Estado);
 
     printf("Digite a Carta em apenas um numero: ");
-    scanf("%s", c1.Carta);
+    scanf("%9s", c1.Carta);
 
     printf("Digite o Codigo da carta: ");
-    scanf("%s", c1.Codigo);
+    scanf("%9s", c1.Codigo);
 
     printf("Digite o Nome da Cidade: ");
     scanf(" %[^\n]", c1.Nome);
@@ -47,10 +54,10 @@ int main() {
     scanf(" %c", &c2.Estado);
 
     printf("Digite a Carta em numero: ");
-    scanf("%s", c2.Carta);
+    scanf("%9s", c2.Carta);
 
     printf("Digite o Codigo da carta: ");
-    scanf("%s", c2.Codigo);
+    scanf("%9s", c2.Codigo);
 
     printf("Digite o Nome da Cidade: ");
     scanf(" %[^\n]", c2.Nome);
@@ -67,14 +74,18 @@ int main() {
     printf("Digite os pontos turisticos em numero: ");
     scanf("%d", &c2.Pontos);
 
-    // ---- Calculos ----
-    float densidade1 = c1.Habitantes / c1.Area;
-    float pibPerCapita1 = (c1.PIB * 1000000000.0) / c1.Habitantes;
+    //PARTE DOS CALCULOS 
+    c1.Densidade = (float)c1.Habitantes / c1.Area;
+    c1.PIBperCapita = (c1.PIB * 1000000000.0f) / c1.Habitantes;
+    c1.SuperPoder = (float)c1.Habitantes + c1.Area + (c1.PIB * 1000000000.0f) + 
+                    c1.Pontos + c1.PIBperCapita + (1.0f / c1.Densidade);
 
-    float densidade2 = c2.Habitantes / c2.Area;
-    float pibPerCapita2 = (c2.PIB * 1000000000.0) / c2.Habitantes;
+    c2.Densidade = (float)c2.Habitantes / c2.Area;
+    c2.PIBperCapita = (c2.PIB * 1000000000.0f) / c2.Habitantes;
+    c2.SuperPoder = (float)c2.Habitantes + c2.Area + (c2.PIB * 1000000000.0f) + 
+                    c2.Pontos + c2.PIBperCapita + (1.0f / c2.Densidade);
 
-    // ---- Mostrar os resultados ----
+    // APRESENTAÇÃO DOS RESULTADOS 
     printf("\n===== Dados Cadastrados =====\n");
 
     printf("\nCidade 1:\n");
@@ -86,8 +97,9 @@ int main() {
     printf("Area: %.2f km²\n", c1.Area);
     printf("PIB: %.2f bilhões\n", c1.PIB);
     printf("Pontos Turisticos: %d\n", c1.Pontos);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
-    printf("PIB per Capita: %.2f R$\n", pibPerCapita1);
+    printf("Densidade Populacional: %.2f hab/km²\n", c1.Densidade);
+    printf("PIB per Capita: %.2f R$\n", c1.PIBperCapita);
+    printf("Super Poder: %.2f\n", c1.SuperPoder);
 
     printf("\nCidade 2:\n");
     printf("Estado: %c\n", c2.Estado);
@@ -98,9 +110,23 @@ int main() {
     printf("Area: %.2f km²\n", c2.Area);
     printf("PIB: %.2f bilhões\n", c2.PIB);
     printf("Pontos Turisticos: %d\n", c2.Pontos);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
-    printf("PIB per Capita: %.2f R$\n", pibPerCapita2);
+    printf("Densidade Populacional: %.2f hab/km²\n", c2.Densidade);
+    printf("PIB per Capita: %.2f R$\n", c2.PIBperCapita);
+    printf("Super Poder: %.2f\n", c2.SuperPoder);
+
+    // ESPAÇO PARA FAZER AS COMPARAÇÕES
+    printf("\n===== Comparações =====\n");
+
+    printf("Habitantes: %d\n", (c1.Habitantes > c2.Habitantes));
+    printf("Área: %d\n", (c1.Area > c2.Area));
+    printf("PIB: %d\n", (c1.PIB > c2.PIB));
+    printf("Pontos Turísticos: %d\n", (c1.Pontos > c2.Pontos));
+    printf("PIB per Capita: %d\n", (c1.PIBperCapita > c2.PIBperCapita));
+    printf("Densidade (menor vence): %d\n", (c1.Densidade < c2.Densidade));
+    printf("Super Poder: %d\n", (c1.SuperPoder > c2.SuperPoder));
 
     return 0;
 }
+
+
 
